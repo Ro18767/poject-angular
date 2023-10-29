de3b356617e9181135ff5a5a6e9e12175b529fcd
Ro18767 <timaropw@gmail.com>
2023-10-29 21:57:54 +0200

    сделал страничку продукта

--- src/app/routes/product/product.component.ts
+++ src/app/routes/product/product.component.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { ProdctsService, Product } from 'src/app/sevice/prodcts.service';
 
 @Component({
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent {
-
+  id!: number;
+  product!: Product;
+  constructor(
+    private prodctsService: ProdctsService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.id = 0;
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      let id_param = params.get('id');
+      if (id_param != null && Number.isInteger(+id_param)) {
+        this.id = +id_param;
+        let product = this.prodctsService.get(this.id)!;
+        if (!product) {
+          this.router.navigate(['']);
+        }
+        this.product = product;
+      } else if (id_param == null) {
+      } else {
+        this.router.navigate(['']);
+      }
+    });
+  }
 }
